6a6679878f5a37639b1257ef0af8f6d2b40eeca0
Laukin <1004320638@qq.com>
2019-10-28 18:01:01 +0800

    .....

--- souhu/src/views/nbaIndex/index.vue
+++ souhu/src/views/nbaIndex/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="nba-home-page">
       <div class="v-tabs-wrap" activecolor="#FFB244">
-        <Header :offset=0></Header>
+        <Header :offset="0"></Header>
         <div class="content-wrap">
           <div class="content" style="transform: translateX(0%) translateZ(0px);">
             <div class="v-tab-pane">
@@ -165,8 +165,54 @@
                     </a>
                     <div class="item-wrap oper">
                       <div class="item">
-                        <header class="icon more"></header>
+                        <header class="icon more">
+                          <img src="../../assets/img/headerImg/微信截图_20191028104600_r2_c2.png" alt />
+                        </header>
                         <footer class="title">更多</footer>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="news-list-wrap">
+                  <div>
+                    <div data-spm="fd-d" class="feed-list-wrap">
+                      <div class="req-wrap">
+                        <div class="toutiao-area">
+                          <h3 class="tt">要闻</h3>
+                          <ul class="feed-list">
+                            <li
+                              data-pull-time="1"
+                              data-spm-type="resource"
+                              data-spm-content="a/350096110_458722"
+                              class="feed-item"
+                              data-spm-data="1"
+                            >
+                              <a
+                                href="http://m.sohu.com/a/350096110_458722?scm=0.0.0.0&amp;spm=smwp.fb-nba-home.fd-d.1.1572249036835X6cmDS5"
+                                data-spm-type="content"
+                                data-spm-content="1|458722|350096110||2|"
+                                class="onePic"
+                                data-spm-data="1"
+                              >
+                                <div class="onePic_detail">
+                                  <h3 class="title">前瞻：库里对阵鹈鹕三少 勇士急需首胜稳定军心</h3>
+                                  <div class="item_info">
+                                    <!---->
+                                    <!---->
+                                    <div class="name">NBA广角</div>
+                                    <div data-news_id="350096110" class="time">2小时前</div>
+                                  </div>
+                                </div>
+                                <div
+                                  class="onePic_img"
+                                  data-src="//29e5534ea20a8.cdn.sohucs.com/q_70,c_lfill,w_228,h_148,g_faces/os/news/8037f70f8037c7aa867a4da5ec9c5270.webp"
+                                  lazy="loaded"
+                                  style="background-image: url(&quot;//29e5534ea20a8.cdn.sohucs.com/q_70,c_lfill,w_228,h_148,g_faces/os/news/8037f70f8037c7aa867a4da5ec9c5270.webp&quot;);"
+                                ></div>
+                              </a>
+                            </li>
+                          </ul>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -303,11 +349,15 @@ export default {
   text-align: center;
   max-width: 4em;
 }
-.columns-wrap .navs .item-wrap.oper {
-  float: right;
+.news-list-wrap {
+  position: relative;
 }
-.columns-wrap .navs .item-wrap.oper .item .more {
-  background-size: 60% auto;
-  background-image: url("../../assets/img/headerImg/微信截图_20191028104600_r2_c2.png");
+.feed-list-wrap {
+  position: relative;
+}
+.feed-list-wrap .tt {
+  padding: 0.4rem 0.346667rem 0.173333rem;
+  font-size: 0.48rem;
+  font-weight: bold;
 }
 </style>
