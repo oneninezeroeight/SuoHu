3e5befe889612c6c085646fa335362d0f84536c7
Laukin <1004320638@qq.com>
2019-10-29 09:28:43 +0800

    .....

--- souhu/src/views/nbaph/NbaPh.vue
+++ souhu/src/views/nbaph/NbaPh.vue
@@ -1,9 +1,30 @@
 <template name="component-name">
-    <div>
-      <div class="v-tabs-wrap" activecolor="#FFB244">
-        <Header :offset=2></Header>
+  <div>
+    <div class="v-tabs-wrap" activecolor="#FFB244">
+      <Header :offset="2"></Header>
+      <div class="content-wrap">
+        <div class="v-tab-pane">
+          <div class="team-rank-wrap">
+            <div class="header">
+              <div class="tabs">
+                <div class="tab east active">东部联盟</div>
+                <div class="tab west">西部联盟</div>
+              </div>
+              <div class="titles">
+                <div class="team">
+                  <div class="name">球队</div>
+                </div>
+                <div class="wins">胜</div>
+                <div class="losses">负</div>
+                <div class="winDiff">胜差</div>
+                <div class="winDetail">近况</div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 import Header from "../../components/header/header.vue";
@@ -13,3 +34,78 @@ export default {
   }
 };
 </script>
+<style  scoped>
+.team-rank-wrap .header .tabs {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  padding: 0.32rem 0;
+  font-size: .32rem
+}
+.team-rank-wrap .header .tabs .tab.east {
+    margin-right: 0.213333rem;
+}
+.team-rank-wrap .header .tabs .tab.active {
+    background-color: #ffb244;
+    color: #fff;
+}
+.team-rank-wrap .header .tabs .tab {
+    width: 2.186667rem;
+    height: 0.72rem;
+    text-align: center;
+    line-height: 0.72rem;
+    background-color: #f2f3f5;
+    color: #999999;
+    border-radius: 0.106667rem;
+}
+.team-rank-wrap .header .titles {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    font-size: 0.373333rem;
+    padding-top: 0.16rem;
+    padding-bottom: 0.266667rem;
+    color: #888888;
+}
+.team-rank-wrap .header .titles .team {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-flex: 1;
+    -ms-flex: 1 1 4rem;
+    flex: 1 1 4rem;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+}
+.team-rank-wrap .header .titles .team .name {
+    margin-left: 0.8rem;
+}
+.team-rank-wrap .header .titles .wins {
+    -webkit-box-flex: 1;
+    -ms-flex: 1 1 1.6rem;
+    flex: 1 1 1.6rem;
+    text-align: center;
+}
+.team-rank-wrap .header .titles .losses {
+    -webkit-box-flex: 0;
+    -ms-flex: 0 0 1.6rem;
+    flex: 0 0 1.6rem;
+    text-align: center;
+}
+.team-rank-wrap .header .titles .winDiff {
+    -webkit-box-flex: 0;
+    -ms-flex: 0 0 1.6rem;
+    flex: 0 0 1.6rem;
+    text-align: center;
+}
+.team-rank-wrap .header .titles .winDetail {
+    -webkit-box-flex: 0;
+    -ms-flex: 0 0 1.6rem;
+    flex: 0 0 1.6rem;
+    text-align: center;
+}
+</style>
--- souhu/src/views/nbaplayer/NbaPlayer.vue
+++ souhu/src/views/nbaplayer/NbaPlayer.vue
@@ -1,9 +1,25 @@
 <template name="component-name">
-    <div>
-      <div class="v-tabs-wrap" activecolor="#FFB244">
-        <Header :offset=3></Header>
+  <div>
+    <div class="v-tabs-wrap" activecolor="#FFB244">
+      <Header :offset="3"></Header>
+      <div data-v-58cb0d1e class="item-title">
+        <section data-v-58cb0d1e class="tabs">
+          <div data-v-58cb0d1e class="tab active">得分</div>
+          <div data-v-58cb0d1e class="tab">篮板</div>
+          <div data-v-58cb0d1e class="tab">助攻</div>
+          <div data-v-58cb0d1e class="tab">抢断</div>
+          <div data-v-58cb0d1e class="tab">盖帽</div>
+          <div data-v-58cb0d1e class="tab">失误</div>
+          <div data-v-58cb0d1e class="tab">犯规</div>
+        </section>
+        <div data-v-58cb0d1e class="titles">
+          <div data-v-58cb0d1e class="player">球员</div>
+          <div data-v-58cb0d1e class="team">球队</div>
+          <div data-v-58cb0d1e class="goals">数据</div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 import Header from "../../components/header/header.vue";
@@ -13,3 +29,81 @@ export default {
   }
 };
 </script>
+<style scoped>
+.item-title {
+  -webkit-box-flex: 1;
+  -ms-flex: 1;
+  flex: 1;
+  background-color: #fff;
+}
+.item-title .tabs {
+  padding: 0.32rem 0.4rem 0.266667rem;
+  overflow: hidden;
+  overflow-x: scroll;
+  -webkit-overflow-scrolling: touch;
+  white-space: nowrap;
+  font-size: 0.4rem;
+}
+.item-title .tabs .tab.active {
+  color: #fff;
+  background-color: #ffb244;
+}
+.item-title .tabs .tab {
+  display: -webkit-inline-box;
+  display: -ms-inline-flexbox;
+  display: inline-flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  margin-right: 0.186667rem;
+  width: 1.493333rem;
+  height: 0.693333rem;
+  font-size: 0.346667rem;
+  color: #999999;
+  background-color: #f2f3f5;
+  border-radius: 0.106667rem;
+}
+.item-title .titles {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  height: 0.96rem;
+  padding: 0 0.4rem;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: justify;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  text-align: center;
+  font-size: 0.373333rem;
+  border-bottom: 1px solid #e0e0e0;
+  color: #888888;
+}
+.item-title .titles .player {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-flex: 1;
+    -ms-flex: 1 1 3.733333rem;
+    flex: 1 1 3.733333rem;
+}
+.item-title .titles .team {
+    -webkit-box-flex: 1;
+    -ms-flex: 1 1 2.133333rem;
+    flex: 1 1 2.133333rem;
+    text-align: center;
+}
+.item-title .titles .goals {
+    -webkit-box-flex: 0;
+    -ms-flex: 0 0 1.066667rem;
+    flex: 0 0 1.066667rem;
+    text-align: center;
+}
+</style>
