eb9948e5483d28bd1c172f941d8887b9bd9ff035
Laukin <1004320638@qq.com>
2019-10-29 11:12:19 +0800

    .....

---
+++ souhu/src/components/nbalm/xblm.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="req-wrap">
+    <div class="team-list" data-spm="Western-rank">
+      <a
+        href="http://m.sohu.com/z/nba/team/16?spm=smwp.fb-nba-home.Western-rank.16.1572310964008qsNsZmB"
+        data-spm-data="16"
+      >
+        <div class="team-item high">
+          <div class="team">
+            <div class="rank">1</div>
+            <div
+              class="flag"
+              lazy-progressive="true"
+              data-src="//statics.itc.cn/sports/basketball/NBA/teamicon/16.png"
+              lazy="loaded"
+              style="background-image: url(&quot;//statics.itc.cn/sports/basketball/NBA/teamicon/16.png&quot;);"
+            ></div>
+            <div class="name">森林狼</div>
+          </div>
+          <div class="wins">3</div>
+          <div class="losses">0</div>
+          <div class="winDiff">0</div>
+          <div class="winDetail">3连胜</div>
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+<style scoped>
+.team-rank-wrap .team-list .team-item.high {
+  background-color: #fff8f0;
+}
+.team-rank-wrap .team-list .team-item {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  height: 1.333333rem;
+  border-bottom: 1px solid #f2f3f5;
+  font-size: 0.373333rem;
+}
+.team-rank-wrap .team-list .team-item .team {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-flex: 1;
+  -ms-flex: 1 1 4rem;
+  flex: 1 1 4rem;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  font-size: 0.4rem;
+}
+.team-rank-wrap .team-list .team-item.high .team .rank {
+  color: #f90d1b;
+}
+.team-rank-wrap .team-list .team-item .team .rank {
+  margin-right: 0.053333rem;
+  min-width: 0.693333rem;
+  color: #999;
+  text-align: center;
+}
+.team-rank-wrap .team-list .team-item .team .flag {
+  margin-right: 0.186667rem;
+  width: 0.533333rem;
+  height: 0.533333rem;
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: contain;
+}
+.team-rank-wrap .team-list .team-item .wins {
+  -webkit-box-flex: 1;
+  -ms-flex: 1 1 1.6rem;
+  flex: 1 1 1.6rem;
+  text-align: center;
+}
+.team-rank-wrap .team-list .team-item .losses {
+  -webkit-box-flex: 0;
+  -ms-flex: 0 0 1.6rem;
+  flex: 0 0 1.6rem;
+  text-align: center;
+}
+.team-rank-wrap .team-list .team-item .winDiff {
+  -webkit-box-flex: 0;
+  -ms-flex: 0 0 1.6rem;
+  flex: 0 0 1.6rem;
+  text-align: center;
+}
+.team-rank-wrap .team-list .team-item .winDetail {
+  -webkit-box-flex: 0;
+  -ms-flex: 0 0 1.6rem;
+  flex: 0 0 1.6rem;
+  text-align: center;
+}
+</style>
--- souhu/src/views/nbaph/NbaPh.vue
+++ souhu/src/views/nbaph/NbaPh.vue
@@ -7,8 +7,8 @@
           <div class="team-rank-wrap">
             <div class="header">
               <div class="tabs">
-                <div class="tab east active">东部联盟</div>
-                <div class="tab west">西部联盟</div>
+                <div class="tab east" :class="{active:shows==1}" @click="toggleTab('DbLm')">东部联盟</div>
+                <div class="tab west" :class="{active:shows==2}" @click="toggleTab('XbLm')">西部联盟</div>
               </div>
               <div class="titles">
                 <div class="team">
@@ -20,6 +20,7 @@
                 <div class="winDetail">近况</div>
               </div>
             </div>
+            <prince :is="currentTab" keep-alive></prince>
           </div>
         </div>
       </div>
@@ -28,9 +29,30 @@
 </template>
 <script>
 import Header from "../../components/header/header.vue";
+import DbLm from "../../components/nbalm/dblm.vue";
+import XbLm from "../../components/nbalm/xblm.vue";
 export default {
+  name: "app",
+  data() {
+    return {
+      currentTab: "DbLm",
+      shows: 1
+    };
+  },
   components: {
-    Header
+    Header,
+    DbLm,
+    XbLm
+  },
+  methods: {
+    toggleTab: function(tab) {
+      this.currentTab = tab;
+      if (this.currentTab == "DbLm") {
+        this.shows = 1;
+      } else {
+        this.shows = 2;
+      }
+    }
   }
 };
 </script>
@@ -43,69 +65,69 @@ export default {
   -ms-flex-pack: center;
   justify-content: center;
   padding: 0.32rem 0;
-  font-size: .32rem
+  font-size: 0.32rem;
 }
 .team-rank-wrap .header .tabs .tab.east {
-    margin-right: 0.213333rem;
+  margin-right: 0.213333rem;
 }
 .team-rank-wrap .header .tabs .tab.active {
-    background-color: #ffb244;
-    color: #fff;
+  background-color: #ffb244;
+  color: #fff;
 }
 .team-rank-wrap .header .tabs .tab {
-    width: 2.186667rem;
-    height: 0.72rem;
-    text-align: center;
-    line-height: 0.72rem;
-    background-color: #f2f3f5;
-    color: #999999;
-    border-radius: 0.106667rem;
+  width: 2.186667rem;
+  height: 0.72rem;
+  text-align: center;
+  line-height: 0.72rem;
+  background-color: #f2f3f5;
+  color: #999999;
+  border-radius: 0.106667rem;
 }
 .team-rank-wrap .header .titles {
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    font-size: 0.373333rem;
-    padding-top: 0.16rem;
-    padding-bottom: 0.266667rem;
-    color: #888888;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  font-size: 0.373333rem;
+  padding-top: 0.16rem;
+  padding-bottom: 0.266667rem;
+  color: #888888;
 }
 .team-rank-wrap .header .titles .team {
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-flex: 1;
-    -ms-flex: 1 1 4rem;
-    flex: 1 1 4rem;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-flex: 1;
+  -ms-flex: 1 1 4rem;
+  flex: 1 1 4rem;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
 }
 .team-rank-wrap .header .titles .team .name {
-    margin-left: 0.8rem;
+  margin-left: 0.8rem;
 }
 .team-rank-wrap .header .titles .wins {
-    -webkit-box-flex: 1;
-    -ms-flex: 1 1 1.6rem;
-    flex: 1 1 1.6rem;
-    text-align: center;
+  -webkit-box-flex: 1;
+  -ms-flex: 1 1 1.6rem;
+  flex: 1 1 1.6rem;
+  text-align: center;
 }
 .team-rank-wrap .header .titles .losses {
-    -webkit-box-flex: 0;
-    -ms-flex: 0 0 1.6rem;
-    flex: 0 0 1.6rem;
-    text-align: center;
+  -webkit-box-flex: 0;
+  -ms-flex: 0 0 1.6rem;
+  flex: 0 0 1.6rem;
+  text-align: center;
 }
 .team-rank-wrap .header .titles .winDiff {
-    -webkit-box-flex: 0;
-    -ms-flex: 0 0 1.6rem;
-    flex: 0 0 1.6rem;
-    text-align: center;
+  -webkit-box-flex: 0;
+  -ms-flex: 0 0 1.6rem;
+  flex: 0 0 1.6rem;
+  text-align: center;
 }
 .team-rank-wrap .header .titles .winDetail {
-    -webkit-box-flex: 0;
-    -ms-flex: 0 0 1.6rem;
-    flex: 0 0 1.6rem;
-    text-align: center;
+  -webkit-box-flex: 0;
+  -ms-flex: 0 0 1.6rem;
+  flex: 0 0 1.6rem;
+  text-align: center;
 }
 </style>
