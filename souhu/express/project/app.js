d1fdaf88719d7f0767bd28c9a99bd71426051115
Laukin <1004320638@qq.com>
2019-10-31 19:35:24 +0800

    ......

--- souhu/express/project/app.js
+++ souhu/express/project/app.js
@@ -22,6 +22,7 @@ var NBAXblm = require('./routes/NBA-xb');
 var NBADblm = require('./routes/NBA-db')
 var Login = require('./routes/login')
 var ZC = require('./routes/zc')
+var Register = require('./routes/register')
 var app = express();
 
 // view engine setup
@@ -58,6 +59,7 @@ app.use('/NBAXblm', NBAXblm);
 app.use('/NBADblm', NBADblm);
 app.use('/Login', Login);
 app.use('/ZC', ZC);
+app.use('/Register', Register);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
